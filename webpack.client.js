f90726fa8f26e0ca95ccdc0db0404434d5e83dc3
ret123 <rathish83@gmail.com>
2018-02-19 13:40:29 +0530

    Refactoring done

--- webpack.client.js
+++ webpack.client.js
@@ -1,27 +1,13 @@
 const path=require('path');
-module.exports = {
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
+const config = {
   entry: './src/client/client.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname,'public')
   },
-
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env',{ targets: { browsers: ['last 2 versions'] }}]
-
-          ]
-        }
-      }
-    ]
-  }
 };
+
+module.exports = merge(baseConfig,config);
--- webpack.server.js
+++ webpack.server.js
@@ -1,27 +1,15 @@
 const path=require('path');
-module.exports = {
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
+
+const config = {
   target: 'node',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname,'build')
   },
-
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env',{ targets: { browsers: ['last 2 versions'] }}]
-
-          ]
-        }
-      }
-    ]
-  }
+  externals: [webpackNodeExternals()]
 };
+module.exports = merge(baseConfig,config);
