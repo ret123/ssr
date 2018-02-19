4392b4c070cee5c395d5ec9f2462dc46dbfb07a5
ret123 <rathish83@gmail.com>
2018-02-19 13:10:11 +0530

    Server and client side setup done

---
+++ src/client/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+  <div>
+    <div>I am the best Home Component</div>
+    <button onClick={() => console.log('hi there')}>Press me</button>
+  </div>
+);
+};
+export default Home;
