fa20e4dec204b2f64140c9e9bcaeefcfd323d666
ret123 <rathish83@gmail.com>
2018-02-19 18:50:20 +0530

    Client side rendering for fetching users completed

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from '../client/components/Home';
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import Routes from "../client/Routes";
 
-export default () => {
-  const content = renderToString(<Home />);
-   return `
+export default (req, store) => {
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
+  );
+  return `
     <html>
     <head></head>
     <body>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import express from 'express';
-import renderer from './helpers/renderer';
+import "babel-polyfill";
+import express from "express";
+import renderer from "./helpers/renderer";
+import createStore from "./helpers/createStore";
+
 const app = express();
 
-app.use(express.static('public'));
-app.get('/', (req,res) => {
-
-  res.send(renderer());
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  const store = createStore();
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
